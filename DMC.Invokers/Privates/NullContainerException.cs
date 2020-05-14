c2e969642cb3aa1f9e1d14546f660801f8d0c526
juandrn <juan.drn@yandex.com>
2020-05-14 16:59:23 -0400

    1.20.2 NullContainerException in ContainerReflector to reinit

---
+++ DMC.Invokers/Privates/NullContainerException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DMC.Invokers.Privates
+{
+    [Serializable]
+    public class NullContainerException : Exception
+    {
+        public NullContainerException()
+        {
+        }
+
+        public NullContainerException(string message) : base(message)
+        {
+        }
+
+        public NullContainerException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NullContainerException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
